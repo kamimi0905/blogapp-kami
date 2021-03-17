4d8d6ba5efc24a804dd480464e672b2d618aa17f
かみみ <ka95jo@gmail.com>
2021-03-18 00:01:19 +0900

    add original validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,9 +10,25 @@
 #
 class Article < ApplicationRecord
     validates :title, presence: true
+    validates :title, length: {minimum: 2,maximum: 100}
+    validates :title,format:{ with: /\A(?!\@)/}
+
     validates :content, presence: true
+    validates :content, length: {minimum: 10}
+    validates :content, uniqueness: true
+
+    # 独自
+    validate :validate_title_and_content_length
 
     def desplay_created_at
         I18n.l(self.created_at,format: :default)
+    end
+
+    private
+    def validate_title_and_content_length
+        char_count = self.title.length + self.content.length
+        if char_count <= 100
+            errors.add(:content, '100文字以上で入力！')
+        end
     end
 end
