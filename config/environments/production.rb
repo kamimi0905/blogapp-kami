5c817991afff20df0c540a7183fcc7fa9df8427e
kamicd <ka95jo@gmail.com>
2021-05-08 13:09:37 +0900

    heroku メール送信4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
 
-  config.action_mailer.default_url_options = { host: "https://blogapp-pro.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "blogapp-pro.herokuapp.com/" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
