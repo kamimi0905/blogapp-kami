afe869ead3b865b9607d4fde8a1acd4beb13834c
kamicd <ka95jo@gmail.com>
2021-05-08 17:42:50 +0900

    heroku メール送信10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,12 +71,12 @@ Rails.application.configure do
 
   host = 'https://blogapp-pro.herokuapp.com/'
   config.action_mailer.default_url_options = { :host => host }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain => "heroku.com",
-    :address => "smtp.sendgrid.net",
+  # config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'yourdomain.com',
+    :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
